d790e905e33661010ca744f3da6844d44eb7e20e
Tvisha Thakur <yashi.thakur1@gmail.com>
2023-04-03 22:00:11 -0400

    Frontend code
    Frontend code

---
+++ src/main/java/com/Replica1/Webservice.java
@@ -0,0 +1,18 @@
+package com.Replica1;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style=Style.RPC)
+public interface Webservice {
+
+    public String addMovieSlots(String movieID, String movieName, int bookingCapacity);
+    public String removeMovieSlots(String movieID, String movieName);
+    public String listMovieShowsAvailability(String movieName, boolean isOwnClient);
+    public String bookMovieTickets(String customerID, String movieID, String movieName, int numberOfTickets, boolean isOwnClient);
+    public String getBookingSchedule(String customerID, boolean isOwnClient);
+    public String cancelMovieTickets(String customerID, String movieID, String movieName, int numberOfTickets);
+    public String exchangeTickets(String customerID, String oldMovieName, String movieID, String newMovieID, String newMovieName, int numberOfTickets);
+}
