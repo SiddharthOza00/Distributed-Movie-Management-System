abc4d06d8c5413c892085297ebd6e323b88d0243
SiddharthOza00 <siddharthoza00@gmail.com>
2023-04-05 12:09:48 -0400

    Front end

--- src/main/java/com/Sequencer/Sequencer.java
+++ src/main/java/com/Sequencer/Sequencer.java
@@ -8,9 +8,9 @@ import java.net.SocketException;
 
 public class Sequencer {
     private static int sequencerId = 0;
-    private static final String sequencerIP = "192.168.238.53";
+    private static final String sequencerIP = "192.168.247.53";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (DatagramSocket aSocket = new DatagramSocket(2233, InetAddress.getByName(sequencerIP))) {
             byte[] buffer = new byte[1000];
             System.out.println("Sequencer UDP Server Started");
@@ -18,10 +18,10 @@ public class Sequencer {
                 DatagramPacket request = new DatagramPacket(buffer,
                         buffer.length);
 
-//                aSocket.receive(request);
-
+                aSocket.receive(request);
                 String sentence = new String(request.getData(), 0,
                         request.getLength());
+                System.out.println(sentence);
 
                 //new comment
 //                String[] parts = sentence.split(";");
@@ -37,10 +37,10 @@ public class Sequencer {
 //                        parts[7] + ";" +
 //                        parts[8] + ";" +
 //                        parts[9] + ";";
-
-                System.out.println(sentence);
-                sendMessage(sentence, 0, true);
-
+//
+//                System.out.println(sentence);
+//                sendMessage(sentence1, sequencerId1, parts[2].equalsIgnoreCase("00"));
+//
 //                byte[] SeqId = (Integer.toString(sequencerId)).getBytes();
 //                InetAddress aHost1 = request.getAddress();
 //                int port1 = request.getPort();
@@ -49,33 +49,36 @@ public class Sequencer {
 //                DatagramPacket request1 = new DatagramPacket(SeqId,
 //                        SeqId.length, aHost1, port1);
 //                aSocket.send(request1);
+//            }
+//
+//        } catch (SocketException e) {
+//            System.out.println("Socket: " + e.getMessage());
+//        } catch (IOException e) {
+//            System.out.println("IO: " + e.getMessage());
+//        }
             }
 
-        } catch (SocketException e) {
-            System.out.println("Socket: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("IO: " + e.getMessage());
-        }
-    }
-
-    public static void sendMessage(String message, int sequencerId1, boolean isRequest) {
-        int port = 5555;
-
-        if (sequencerId1 == 0 && isRequest) {
-            sequencerId1 = ++sequencerId;
-        }
-        String finalMessage = "this is a test";
+//    public static void sendMessage(String message, int sequencerId1, boolean isRequest) {
+//        int port = 5555;
+//
+//        if (sequencerId1 == 0 && isRequest) {
+//            sequencerId1 = ++sequencerId;
+//        }
+////        String finalMessage = "this is a test";
 //        String finalMessage = sequencerId1 + ";" + message;
-        try (DatagramSocket aSocket = new DatagramSocket()) {
-
-            byte[] messages = finalMessage.getBytes();
-            InetAddress aHost = InetAddress.getByName("228.5.6.7");
-
-            DatagramPacket request = new DatagramPacket(messages,
-                    messages.length, aHost, port);
-            aSocket.send(request);
-        } catch (IOException e) {
-            e.printStackTrace();
+//        try (DatagramSocket aSocket = new DatagramSocket()) {
+//
+//            byte[] messages = finalMessage.getBytes();
+//            InetAddress aHost = InetAddress.getByName("228.5.6.7");
+//
+//            DatagramPacket request = new DatagramPacket(messages,
+//                    messages.length, aHost, port);
+//            aSocket.send(request);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
         }
     }
 }
