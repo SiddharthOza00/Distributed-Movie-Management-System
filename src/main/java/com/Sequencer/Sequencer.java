5823a832199c287c1925a36997fb29dfc61472b7
SiddharthOza00 <siddharthoza00@gmail.com>
2023-04-07 19:19:03 -0400

    Sequencer

--- src/main/java/com/Sequencer/Sequencer.java
+++ src/main/java/com/Sequencer/Sequencer.java
@@ -19,28 +19,26 @@ public class Sequencer {
                         buffer.length);
 
                 aSocket.receive(request);
-                String sentence = new String(request.getData(), 0,
-                        request.getLength());
-                System.out.println(sentence);
+                String sentence = new String(request.getData(), 0, request.getLength());
+                sendMessage(sentence,0);
+//                System.out.println(sentence);
 
                 //new comment
-//                String[] parts = sentence.split(";");
-//                int sequencerId1 = Integer.parseInt(parts[0]);
+//                String[] parts = sentence.split(",");
+//                int sequencerId1 = Integer.parseInt(parts[7]);
 //                String ip = request.getAddress().getHostAddress();
 //
-//                String sentence1 = ip + ";" +
-//                        parts[2] + ";" +
-//                        parts[3] + ";" +
-//                        parts[4] + ";" +
-//                        parts[5] + ";" +
-//                        parts[6] + ";" +
-//                        parts[7] + ";" +
-//                        parts[8] + ";" +
-//                        parts[9] + ";";
-//
+//                String sentence1 = ip + "," +
+//                        parts[2] + "," +
+//                        parts[3] + "," +
+//                        parts[4] + "," +
+//                        parts[5] + "," +
+//                        parts[6] + "," +
+//                        parts[7] + ",";
+////
 //                System.out.println(sentence);
-//                sendMessage(sentence1, sequencerId1, parts[2].equalsIgnoreCase("00"));
-//
+//                sendMessage(sentence1, sequencerId1);
+////
 //                byte[] SeqId = (Integer.toString(sequencerId)).getBytes();
 //                InetAddress aHost1 = request.getAddress();
 //                int port1 = request.getPort();
@@ -49,36 +47,37 @@ public class Sequencer {
 //                DatagramPacket request1 = new DatagramPacket(SeqId,
 //                        SeqId.length, aHost1, port1);
 //                aSocket.send(request1);
-//            }
-//
-//        } catch (SocketException e) {
-//            System.out.println("Socket: " + e.getMessage());
-//        } catch (IOException e) {
-//            System.out.println("IO: " + e.getMessage());
-//        }
             }
 
-//    public static void sendMessage(String message, int sequencerId1, boolean isRequest) {
-//        int port = 5555;
-//
-//        if (sequencerId1 == 0 && isRequest) {
-//            sequencerId1 = ++sequencerId;
-//        }
-////        String finalMessage = "this is a test";
-//        String finalMessage = sequencerId1 + ";" + message;
-//        try (DatagramSocket aSocket = new DatagramSocket()) {
-//
-//            byte[] messages = finalMessage.getBytes();
-//            InetAddress aHost = InetAddress.getByName("228.5.6.7");
-//
-//            DatagramPacket request = new DatagramPacket(messages,
-//                    messages.length, aHost, port);
-//            aSocket.send(request);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
         }
     }
+
+    public static void sendMessage(String message, int sequencerId1) {
+        int port = 5555;
+
+        if (sequencerId1 == 0) {
+            sequencerId1 = ++sequencerId;
+        }
+//        String finalMessage = "this is a test";
+//        String finalMessage = message + "," + sequencerId1;
+          String finalMessage = message;
+        try (DatagramSocket aSocket = new DatagramSocket()) {
+
+            byte[] messages = finalMessage.getBytes();
+            InetAddress aHost = InetAddress.getByName("228.5.6.7");
+
+            DatagramPacket request = new DatagramPacket(messages,
+                    messages.length, aHost, port);
+            aSocket.send(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+//}
+//        }
+//    }
 }
