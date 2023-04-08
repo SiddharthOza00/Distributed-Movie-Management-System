7f0a609d3c3ddd44c59f82e50f63ab3ed299c12b
Dhyey Doshi <dhyeydoshi2512@gmail.com>
2023-04-07 23:14:20 -0400

    Webserv update in RM

---
+++ src/main/java/com/Replica3/Testing.java
@@ -0,0 +1,15 @@
+package com.Replica3;
+
+import java.util.Scanner;
+
+public class Testing {
+    public static void main(String[] args) throws Exception {
+        LaunchServer.main(args);
+
+        Scanner sc = new Scanner(System.in);
+
+        // while(true) {
+        //     if(s)
+        // }
+    }
+}
